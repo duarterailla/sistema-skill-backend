c2ec686f76ee3c502afe134c2511d34e4e4e66e4
Raílla Duarte <railladuarte@hotmail.com>
2025-08-13 16:56:14 -0300

    Add files via upload

---
+++ backend-web/src/main/java/com/frontend-web/sistemaskill/SistemaSkillApplication.java
@@ -0,0 +1,20 @@
+package com.neki.sistemaskill;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaSkillApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaSkillApplication.class, args);
+		System.out.println("Back rodando corretamente");
+	}
+	
+	public void exemploJson() {
+		String json = "{\n" +
+				"  \"nome\": \"Java\",\n" +
+				"  \"descricao\": \"Linguagem de programação\",\n" +
+				"  \"imagem_url\": \"https://exemplo.com/java.png\"\n" +
+				"}";
+		System.out.println(json);
+	}
+}
---
+++ backend-web/src/main/java/com/frontend-web/sistemaskill/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.neki.sistemaskill.repository;
+
+import com.neki.sistemaskill.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByLogin(String login);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByNickname(String nickname);
+}
